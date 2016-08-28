e53a42cc99e2448d8fbcda6b6eac78ff764e3bc2
Ian Seckington <iseckington@aifs.co.uk>
2016-08-28 21:14:27 +0100

    nancy connect to local mongodb, dockerisation of release build

--- server/toq/Module/IndexModule.cs
+++ server/toq/Module/IndexModule.cs
@@ -3,13 +3,28 @@
     using System;
     using System.Collections.Generic;
     using Nancy;
+    using MongoDB.Driver;
 
     public class IndexModule : NancyModule
     {
         public IndexModule()
         {
+            _client = new MongoClient(new MongoUrl("mongodb://192.168.0.26/"));
+            _database = _client.GetDatabase("test");
+
             Get["/"] = _ => "Nancy: Hello";
             Get["/os"] = _ => Environment.OSVersion.ToString();
+            Get["mongo"] = GetMongoStatus;
         }
+
+        private dynamic GetMongoStatus(dynamic arg)
+        {
+
+
+            return HttpStatusCode.OK;
+        }
+
+        protected static IMongoClient _client;
+        protected static IMongoDatabase _database;
     }
 }
